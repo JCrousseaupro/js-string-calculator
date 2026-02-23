15f797e10e1ad9981fd63f7e620ad1f6ef86dd10
Jean-Charles <jean-charles.rousseau@trepied.eu>
2026-02-23 16:14:22 +0100

    init

---
+++ stringCalculator.js
@@ -0,0 +1,48 @@
+
+
+
+export function add(str) {
+    if (str.length === 0) return 0;
+    if (!isNaN(str)) return parseInt(str);
+
+    let delimiters = getDelimiters(str);
+    str = deleteDelimiter(str);
+
+    delimiters.forEach(delimiter => {
+        str = str.replaceAll(delimiter, ',');
+    });
+
+    let numbers = str
+        .replaceAll('\n', ',')
+        .split(',')
+        .map(Number)
+        .filter(elem => elem <= 1000);
+
+    let negatives = numbers.filter(elem => elem < 0);
+    if (negatives.length > 0) throw Error(`Negatives not allowed. [${negatives.join(',')}]`);
+
+    return numbers.reduce((sum, current) => sum + current);
+}
+
+function hasDelimiter(str) {
+    return (str.includes('//'));
+}
+
+function getDelimiters(str) {
+    if (hasDelimiter(str)) {
+        if (str.includes('['))
+            return str.split(/\[(.*?)\]/) // Get delimiters between square brackets
+                .filter((elem, key) => (key % 2 !== 0));
+        else
+            return [str.charAt(2)];
+    }
+
+    return [','];
+}
+
+function deleteDelimiter(str) {
+    if (hasDelimiter(str))
+        return str.split('\n', 2)[1]
+
+    return str;
+}
